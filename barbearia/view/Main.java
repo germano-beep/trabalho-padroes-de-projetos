6d4ba85ddd533c2b896f611090ede1593a19c4f5
flaviane <flaviane.ferrares@gmail.com>
2022-04-12 11:04:54 -0300

    update Main

--- barbearia/view/Main.java
+++ barbearia/view/Main.java
@@ -1,6 +1,9 @@
 package barbearia.view;
 
+import barbearia.Model.Client;
+import barbearia.Model.Schedule;
 import barbearia.Model.Service;
+import barbearia.Model.User;
 /**
  * @author flaviane ferrares
  */
@@ -8,14 +11,23 @@ import barbearia.Model.Service;
 public class Main {
     
     public static void main(String[] args) {
-        
-        String nome = "Flaviane";
-        System.out.println(nome);
 
-        Service service = new Service(1, "Barba", 10);
+        Service servico = new Service(1, "Barba", 10);
+        //System.out.println(servico.getDescricao());
+        //System.out.println(servico.getValor());
 
-        System.out.println(service.getDescricao());
-        System.out.println(service.getValor());
+        Client cliente = new Client(1, "Flaviane", "Teste", "1111111");
+        //System.out.println(cliente.getNome());
+
+        User usuario = new User(1, "Barbeiro", "senha");
+        //System.out.println(usuario.getNome());
+
+        Schedule agenda = new Schedule(1, cliente, servico, 10, "12/04/2022 10:55");
+        System.out.println(agenda.getId());
+        System.out.println(agenda.getCliente().getNome());
+        System.out.println(agenda.getServico().getDescricao());
+        System.out.println(agenda.getValor());
+        System.out.println(agenda.getData());
 
 
     }
